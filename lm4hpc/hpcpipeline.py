7aa32bc87925813c2113dea5a35e2fda7f755862
LChenGit <lechen@iastate.edu>
2023-08-29 14:22:58 -0700

    updated with lambda function

--- lm4hpc/hpcpipeline.py
+++ lm4hpc/hpcpipeline.py
@@ -3,7 +3,7 @@ from .pipeline_openmp_question_answering import openmp_question_answering
 
 def hpcpipelines(task, model, **kwargs):
     if task == 'openmp_question_answering':
-        return openmp_question_answering(model, **kwargs)
+        return lambda question: openmp_question_answering(model, question)
     # elif task == 'similarity_checking':
     #     return similarity_checking(model, **kwargs)
     # elif task == 'codebase_question_answering':
