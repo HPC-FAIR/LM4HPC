31256315e36252391c1c5adde9b40c5954629b4c
LChenGit <lechen@iastate.edu>
2023-08-29 14:06:47 -0700

    fix project name

---
+++ lm4hpc/__init__.py
@@ -0,0 +1,1 @@
+from .hpcpipeline import hpcpipelines
--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='lm4hpc',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='./lm4hpc'),
+    package_dir={'': '.',
+                 "lm4hpc": "./lm4hpc"},
     install_requires=[
-        # list of required packages
+        'torch',
+        'transformers',
     ],
 )
