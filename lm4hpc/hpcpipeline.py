89d1c12eaae97dc3a6b76fae52eb7fc0aa9f39fe
LChenGit <lechen@iastate.edu>
2023-08-29 15:48:46 -0700

    update config

--- lm4hpc/hpcpipeline.py
+++ lm4hpc/hpcpipeline.py
@@ -1,12 +1,45 @@
+import json
+import os
 from .pipeline_openmp_question_answering import openmp_question_answering
+from .pipeline_codebase_question_answering import codebase_question_answering
+from .pipeline_similarity_checking import similarity_checking
 
+def hpcpipelines(task: str, model: str, **kwargs) -> callable:
+    """
+    Returns a function for the specified task and model.
 
-def hpcpipelines(task, model, **kwargs):
-    if task == 'openmp_question_answering':
-        return lambda question: openmp_question_answering(model, question)
-    # elif task == 'similarity_checking':
-    #     return similarity_checking(model, **kwargs)
-    # elif task == 'codebase_question_answering':
-    #     return codebase_question_answering(model, **kwargs)
-    else:
+    Parameters:
+        task (str): The task to perform. Options are 'openmp_question_answering', 'similarity_checking', and 'codebase_question_answering'.
+        model (str): The model to use for the task.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        callable: A function that takes the appropriate parameters for the specified task and returns the result.
+
+    Raises:
+        ValueError: If the task or model is not valid.
+    """
+    # Read the configuration file
+    with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
+        config = json.load(f)
+    
+    # Check if the task is valid
+    if task not in config:
         raise ValueError('Unknown task: {}'.format(task))
+    
+    # Check if the model is valid for the task
+    if model not in config[task]['models']:
+        raise ValueError('Invalid model for {}: {}'.format(task, model))
+    
+    # Get the default parameters for the model
+    default_parameters = config[task]['default_parameters'][model]
+    
+    # Update the default parameters with the user-specified parameters
+    parameters = {**default_parameters, **kwargs}
+    
+    if task == 'openmp_question_answering':
+        return lambda question: openmp_question_answering(model, question, **parameters)
+    elif task == 'similarity_checking':
+        return lambda code1, code2: similarity_checking(model, code1, code2, **parameters)
+    elif task == 'codebase_question_answering':
+        return lambda question: codebase_question_answering(model, question, **parameters)
--- lm4hpc/pipeline_openmp_question_answering.py
+++ lm4hpc/pipeline_openmp_question_answering.py
@@ -1,16 +1,23 @@
-
 import torch
 from transformers import pipeline
 
-def openmp_question_answering(model, question):
+def openmp_question_answering(model: str, question: str, **parameters) -> str:
+    """
+    Generates an answer to a question using the specified model and parameters.
+
+    Parameters:
+        model (str): The model to use for question answering. Options are 'databricks/dolly-v2-12b', 'gpt3', and 'starcoder'.
+        question (str): The question to answer.
+        **parameters: Additional keyword arguments to pass to the `pipeline` function.
+
+    Returns:
+        str: The generated answer.
+
+    Raises:
+        ValueError: If the model is not valid.
+    """
     if model == 'databricks/dolly-v2-12b':
-        text_generator = pipeline(model=model,
-                                torch_dtype=torch.bfloat16,
-                                trust_remote_code=True,
-                                device_map="auto",
-                                max_new_tokens=256,
-                                temperature=0.001,
-                                return_full_text=True)
+        text_generator = pipeline(model=model, **parameters)
         return text_generator(question)[0]["generated_text"].split("\n")[-1]
     elif model == 'gpt3':
         # initialize gpt3 model
