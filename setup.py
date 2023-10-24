5068085a532feb15b4ed4a7e8de75b063569b080
LChenGit <lechen@iastate.edu>
2023-10-24 16:09:49 -0500

    added codellama

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='lm4hpc',
     version='0.1',
-    packages=find_packages(where='./lm4hpc'),
-    package_dir={'': '.',
-                 "lm4hpc": "./lm4hpc"},
+    packages=find_packages(),
+    package_dir={'': '.'},
     package_data={'lm4hpc': ['config.json']},
     install_requires=[
         'torch',
